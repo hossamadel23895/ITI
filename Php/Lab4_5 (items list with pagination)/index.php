ea729a1c361bb71b42832094ce0376642d237991
hossamadel23895 <hossamadel23895@gmail.com>
2022-03-09 18:19:55 +0200

    added lab4-5 php

--- Php/Lab4_5 (items list with pagination)/index.php
+++ Php/Lab4_5 (items list with pagination)/index.php
@@ -2,21 +2,20 @@
 session_start();
 require_once "vendor/autoload.php";
 
-use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Database\Capsule\Manager as DBCM;
 
-$capsule = new Capsule();
-$capsule->addConnection([
+$dbcm = new DBCM();
+$dbcm->addConnection([
     "driver" => _driver_,
     "host" => _host_,
     "database" => _database_,
     "username" => _username_,
     "password" => _password_,
 ]);
-$capsule->setAsGlobal();
-$capsule->bootEloquent();
+$dbcm->setAsGlobal();
+$dbcm->bootEloquent();
 $index = (isset($_GET["index"]) && is_numeric($_GET["index"]) && $_GET["index"] > 0) ? (int) $_GET["index"] : 0;
-$all_requrds = Capsule::table("items")->skip($index)->take(_Pager_size_)->get();
-$counter = Capsule::table("items")->skip($index)->take(_Pager_size_)->count();
+$counter = DBCM::table("items")->skip($index)->take(_Pager_size_)->count();
 $next_index = (($index + _Pager_size_) < $counter) ? $index + _Pager_size_ : abs($counter - _Pager_size_);
 $next_link = "http://localhost/Lab4_5/index.php?index=$next_index";
 $previous_index = (($index - _Pager_size_) >= 0) ? $index - _Pager_size_ : 0;
@@ -28,8 +27,8 @@ if (isset($_GET["glass"]) && is_numeric($_GET["glass"]) && $_GET["glass"] >= 0) 
     require_once "views/table.php";
 }
 if (isset($_POST['item'])) {
-    $result = Capsule::table("items")->where("product_name", $_POST['item'])->first();
-    if (Capsule::table('items')->where('product_name', $_POST['item'])->exists()) {
+    $result = DBCM::table("items")->where("product_name", $_POST['item'])->first();
+    if (DBCM::table('items')->where('product_name', $_POST['item'])->exists()) {
         require_once "views/result.php";
     } else {
         require_once "views/table.php";
